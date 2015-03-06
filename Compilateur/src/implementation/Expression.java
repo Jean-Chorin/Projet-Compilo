ab0c34e8400c23eeb4c778829d14ae837538b1c2
Zeldarck <zeldarck.martin@gmail.com>
2015-03-06 17:36:57 +0100

    2

--- Compilateur/src/implementation/Expression.java
+++ Compilateur/src/implementation/Expression.java
@@ -3,9 +3,10 @@ import java.util.Stack;
 
 import main.*;
 
-enum Operateur{"+","-","*","/"};
+
 
 public class Expression {
+	
 	private Stack<Ident> pile_Ident;
 	private Stack<Operateur> pile_op;
 }

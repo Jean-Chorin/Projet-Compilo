587e21dcd9ade139bf0d74f99d236510e7151a47
Zeldarck <zeldarck.martin@gmail.com>
2015-03-06 17:35:35 +0100

    commencement

--- Compilateur/src/implementation/Expression.java
+++ Compilateur/src/implementation/Expression.java
@@ -1,5 +1,11 @@
 package implementation;
+import java.util.Stack;
+
+import main.*;
+
+enum Operateur{"+","-","*","/"};
 
 public class Expression {
-
+	private Stack<Ident> pile_Ident;
+	private Stack<Operateur> pile_op;
 }

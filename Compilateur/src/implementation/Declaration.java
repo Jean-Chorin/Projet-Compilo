d31836f3106b4acf1568b6dbb2d5e3df642cbc2e
Jean Chorin <jchorin@insa-rennes.fr>
2015-03-09 09:28:34 +0100

    update Declaration

--- Compilateur/src/implementation/Declaration.java
+++ Compilateur/src/implementation/Declaration.java
@@ -1,37 +1,37 @@
 package implementation;
 
+import ident.*;
+import main.*;
+
 public class Declaration {
 	private int offsetCompteur; //
-	private int type; //booléen ou entier => voir l'enum. Valeur mise en mémoire obligatoirement, sinon on ne sait pas si on a un booleen ou entier
+	private Type type; //booléen ou entier => voir l'enum. Valeur mise en mémoire obligatoirement, sinon on ne sait pas si on a un booleen ou entier
 	
 	public Declaration() {
 		super();
 		offsetCompteur = -2;
 	}
 	
-	//création d'une constante booléenne
-	public void addConst(String nom , String valeur) {
-		tabIdent.rangeIdent(nom , new IdConst(nom , "BOOLEEN" , valeur));
+	//création d'une constante booléenne de type t
+	public void addConst(String nom , Type t , String valeur) throws ParseException{
+		if (existeIdent(nom)) {
+			throw Yaka.generateParseException();
+		}
+		Yaka.tabIdent.rangeIdent(nom , new IdConst(nom , t , valeur));
 	}
 	
-	//création d'une constante entiere
-	public void addConst(String nom , int valeur) {
-		tabIdent.rangeIdent(nom , new IdConst(nom , "ENTIER" , valeur));
-	}
-	
-	//change le type des prochaines valeurs entrées en ENTIER 
-	public void setEntier() {
-		type = ENTIER;
-	}
-
-	//change le type des prochaines valeurs entrées en BOOLEEN
-	public void setBooleen() {
-		type = BOOLEEN;
+	//change le type des prochaines valeurs entrées en t
+	public void setType(Type t) {
+		type = t;
 	}
 	
 	//ajoute un Ident de nom "nom" et de type Declaration.type 
-	public void addIdent(String nom) {
-		
+	public void addIdent(String nom) throws ParseException{
+		if (existeIdent(nom)) {
+			throw Yaka.generateParseException();
+		}
+		Yaka.tabIdent.rangeIdent(nom , new IdVar(nom , t , offsetCompteur));
+		offsetCompteur -= 2;
 	}
 
 }

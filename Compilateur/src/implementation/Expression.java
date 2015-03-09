4679a5ee2565256fa5199834251a85e2cba48154
zeldarck <zeldarck.martin@gmail.com>
2015-03-09 08:37:34 +0100

    commencement Expr

--- Compilateur/src/implementation/Expression.java
+++ Compilateur/src/implementation/Expression.java
@@ -1,6 +1,6 @@
 package implementation;
 import java.util.Stack;
-
+import ident.*;
 import main.*;
 
 
@@ -9,4 +9,26 @@ public class Expression {
 	
 	private Stack<Ident> pile_Ident;
 	private Stack<Operateur> pile_op;
+	private Ident destination; //où on stock le calcul
+	
+	public Expression(){
+		pile_Ident = new Stack<Ident>();
+		pile_op = new Stack<Operateur>();
+	}
+	
+	public void empileOp(Operateur o){
+		pile_op.push(o);
+	}
+	
+	public void empileIdent(Ident e){
+		pile_Ident.push(e);
+	}
+	
+	public void addDest(Ident e){
+		destination = e;
+	}
+	
+	public void calcul(){
+		
+	}
 }

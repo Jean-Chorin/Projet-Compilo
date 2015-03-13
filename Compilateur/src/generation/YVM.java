ac108cb53f3c1b8536442ec6fd3957c8a2f41f4a
Jean Chorin <jchorin@insa-rennes.fr>
2015-03-13 16:24:34 +0100

    initialisation de YVM

--- Compilateur/src/generation/YVM.java
+++ Compilateur/src/generation/YVM.java
@@ -1,8 +1,15 @@
 package generation;
+
 import java.io.*;
 
+
 public class YVM {
+	private OutputStream sortie;
 	
-	OutputStream sortie;
+	//Ouvre un OutputStream vers le fichier de nom "Alpha"
+	public YVM() {
+		sortie = Ecriture.ouvrir("Alpha");
+	}
+	
 	
 }

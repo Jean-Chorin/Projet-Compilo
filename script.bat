96951d02e2af0fb7a5498019ff57680cd5f91721
Jean Chorin <jchorin@insa-rennes.fr>
2015-03-14 20:25:18 +0100

    amelioration script.bat : texte echo

--- script.bat
+++ script.bat
@@ -1,11 +1,16 @@
+echo "-------------------------------git add : -------------------------------"
 git add .
 git status
 set /p message= message du commit :
+echo "-------------------------------git commit : -------------------------------"
 git commit -m "%message%"
 git status
+echo "-------------------------------git fetch : -------------------------------"
 git fetch
 git status
+echo "-------------------------------git rebase : -------------------------------"
 git rebase
 git status
+echo "-------------------------------git push : -------------------------------"
 git push
 git status

1a4e324918d8e68dbc9cd4763c865665aa5b48be
Jean Chorin <jchorin@insa-rennes.fr>
2015-03-14 20:20:32 +0100

    amelioration script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo -n "Message : "
 read message
+echo "git add : "
 git add .
 git status
+echo "git commit : "
 git commit -m "$message"
 git status
+echo "git fetch : "
 git fetch
 git status
+echo "git rebase : "
 git rebase
 git status
+echo "git push : "
 git push
 git status

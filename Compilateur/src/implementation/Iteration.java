bba93cb065e74952462666d70e36a55717f9acd0
zeldarck <zeldarck.martin@gmail.com>
2015-03-16 17:56:24 +0100

    iteration qui avance

--- Compilateur/src/implementation/Iteration.java
+++ Compilateur/src/implementation/Iteration.java
@@ -1,28 +1,34 @@
 package implementation;
 
+import java.util.Stack;
+
+import main.Yaka;
+import ident.*;
+
 public class Iteration {
-	
+	private Stack<Integer> niv;
+	private int nivMax;
 	
 	public Iteration() {
-		
+		niv = new Stack<Integer>();
+		nivMax = 0;
 	}
 	
 	public void tantque() {
-		
+		nivMax++;
+		niv.push(nivMax);
+		Yaka.yvm.tantque(nivMax);
 	}
 	
-	public void iffaux() {
-		
-	}
-	
-	public void goTo() {
-		
+	public void iffaux(Type t) {
+		if(t == Type.BOOLEEN){
+			Yaka.yvm.iffaux(000000000000000);
+		}else{
+			System.out.println("Veuillez passer une expression bolleenne sue laquelle iterer");
+		}
 	}
 	
 	public void fait() {
 		
 	}
-	
-	
-	
 }

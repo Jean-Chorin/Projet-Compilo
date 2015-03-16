636073323edcd54c3585876c672494b3162b455c
Fermivor <fermivor@gmail.com>
2015-03-16 17:56:42 +0100

    sortie.asm

--- Compilateur/sortie.asm
+++ Compilateur/sortie.asm
@@ -9,3 +9,131 @@ extrn ecrch:proc, ligsuiv:proc
 debut :
 STARTUPCODE
 
+; ouvrePrinc 14
+mov bp,sp
+sub sp,14
+
+; ecrireChaine "c1="
+.DATA
+mess0 DB "c1=$"
+.CODE
+lea dx,mess0
+push dx
+call ecrch
+
+; lireEnt -2
+lea dx,[bp-2]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "c3="
+.DATA
+mess1 DB "c3=$"
+.CODE
+lea dx,mess1
+push dx
+call ecrch
+
+; lireEnt -6
+lea dx,[bp-6]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "c4="
+.DATA
+mess2 DB "c4=$"
+.CODE
+lea dx,mess2
+push dx
+call ecrch
+
+; lireEnt -8
+lea dx,[bp-8]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+; iload -6
+push word ptr [bp-6]
+
+; iload -8
+push word ptr [bp-8]
+
+; iconst 2
+push 2
+
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
+
+; iadd
+pop bx
+pop ax
+add ax,bx
+push ax
+
+; iconst 5
+push 5
+
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
+
+; ecrireEnt
+call ecrent
+
+; aLaLigne
+call ligsuiv
+
+; iload -2
+push word ptr [bp-2]
+
+; iconst 3
+push 3
+
+; iload -2
+push word ptr [bp-2]
+
+; imul
+pop bx
+pop ax
+imul bx
+push ax
+
+; iadd
+pop bx
+pop ax
+add ax,bx
+push ax
+
+; iconst 10
+push 10
+
+; isub
+pop bx
+pop ax
+sub ax,bx
+push ax
+
+; ecrireEnt
+call ecrent
+
+; queue
+nop
+exitcode
+end debut
+

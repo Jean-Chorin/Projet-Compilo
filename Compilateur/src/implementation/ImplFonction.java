7ac5cdd1035b190ee2bb1d2b64c11cb6dd597e88
Fermivor <fermivor@gmail.com>
2015-03-31 14:09:46 +0200

    fonctions

--- Compilateur/src/implementation/ImplFonction.java
+++ Compilateur/src/implementation/ImplFonction.java
@@ -1,5 +1,26 @@
 package implementation;
 
-public class ImplFonction {
+import ident.*;
+import main.ParseException;
+import main.Yaka;
 
+public class ImplFonction {
+	
+	
+	public void retourne(Type t){
+		Type f;
+			f = Yaka.tabIdent.chercheGlobal(Yaka.tabIdent.last).resultat;
+		if(t == f){
+			Yaka.yvm.ireturn(Yaka.tabIdent.globaux.get(Yaka.tabIdent.last).nbParam()*2+4);
+		}else{
+			System.out.println("Incorect parameter Type in the fonction" + Yaka.tabIdent.last + " a la ligne " + 
+					Yaka.token.next.beginLine + " et a la colonne " + Yaka.token.next.beginColumn);
+		}
+	}
+	
+	//public void fermeBloc (){
+	
+	//}
+
+	
 }

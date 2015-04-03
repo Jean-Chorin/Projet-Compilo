b8c8fdffc1863b4c3e7c5cf2e2861de0708c72b2
Jean Chorin <jchorin@insa-rennes.fr>
2015-04-03 17:08:44 +0200

    envoi

--- Compilateur/sortie.asm
+++ Compilateur/sortie.asm
@@ -61,125 +61,90 @@ FSI1:
 leave
 ret 4
 
-min:
-;ouvcBloc 0
-enter 0,0
+lait:
+;ouvcBloc 6
+enter 6,0
 
-; iload 6
-push word ptr [bp+6]
+; iconst 6
+push word ptr 6
 
-; iload 4
-push word ptr [bp+4]
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
 
-; iinf
+; iload -6
+push word ptr [bp-6]
+
+; iconst 42
+push word ptr 42
+
+; iinfegal
 pop bx
 pop ax
 cmp ax,bx
-jge $+6
+jg $+6
 push -1
 jmp $+4
 push 0
-
-; iffaux SINON2
-pop ax
-cmp ax,0
-je SINON2
-
-; iload 6
-push word ptr [bp+6]
-
-;ireturn 8
-pop ax
-mov [bp+8],ax
-
-; goto FSI2
-jmp FSI2
-
-SINON2:
-; iload 4
-push word ptr [bp+4]
-
-;ireturn 8
-pop ax
-mov [bp+8],ax
-
-FSI2:
-;fermeBloc 4
-leave
-ret 4
-
-sup:
-;ouvcBloc 0
-enter 0,0
-
-; iload 6
-push word ptr [bp+6]
-
-; iload 4
-push word ptr [bp+4]
-
-; isup
-pop bx
-pop ax
-cmp ax,bx
-jle $+6
-push -1
-jmp $+4
-push 0
-
-;ireturn 8
-pop ax
-mov [bp+8],ax
-
-;fermeBloc 4
-leave
-ret 4
-
-debut :
-STARTUPCODE
-
-main:
-;ouvcBloc 8
-enter 8,0
-
-; iconst 5
-push word ptr 5
 
 ; istore -2
 pop ax
 mov word ptr [bp-2],ax
 
-; lireEnt -4
-lea dx,[bp-4]
-push dx
-call lirent
-
-; aLaLigne
-call ligsuiv
+; iload -6
+push word ptr [bp-6]
 
 ;reserveRetour
 sub sp,2
 
-; iload -2
-push word ptr [bp-2]
-
-;reserveRetour
-sub sp,2
-
-; iload -4
-push word ptr [bp-4]
+; iload -6
+push word ptr [bp-6]
 
 ; iconst 5
 push word ptr 5
 
-;call min
-call min
-
 ;call max
 call max
 
-; iconst 2
-push word ptr 2
+; iegal
+pop bx
+pop ax
+cmp ax,bx
+jne $+6
+push -1
+jmp $+4
+push 0
+
+; istore -4
+pop ax
+mov word ptr [bp-4],ax
+
+FAIRE1:
+; iload -6
+push word ptr [bp-6]
+
+; iconst 10
+push word ptr 10
+
+; iinfegal
+pop bx
+pop ax
+cmp ax,bx
+jg $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux FAIT1
+pop ax
+cmp ax,0
+je FAIT1
+
+; iload -6
+push word ptr [bp-6]
+
+; iconst 1
+push word ptr 1
 
 ; iadd
 pop bx
@@ -191,44 +156,93 @@ push ax
 pop ax
 mov word ptr [bp-6],ax
 
-;reserveRetour
-sub sp,2
+FAIT1:
+; iload -2
+push word ptr [bp-2]
 
-; iconst 1
-push word ptr 1
+; iffaux SINON2
+pop ax
+cmp ax,0
+je SINON2
 
-;reserveRetour
-sub sp,2
+; iconst 42
+push word ptr 42
+
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
 
 ; iload -2
 push word ptr [bp-2]
+
+; istore -4
+pop ax
+mov word ptr [bp-4],ax
+
+; goto FSI2
+jmp FSI2
+
+SINON2:
+FSI2:
+; iload -4
+push word ptr [bp-4]
+
+;ireturn 4
+pop ax
+mov [bp+4],ax
+
+;fermeBloc 0
+leave
+ret 0
+
+caramel:
+;ouvcBloc 6
+enter 6,0
+
+; iload 6
+push word ptr [bp+6]
+
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
+
+; iload 4
+push word ptr [bp+4]
+
+; istore -4
+pop ax
+mov word ptr [bp-4],ax
+
+; iload 6
+push word ptr [bp+6]
+
+; iconst 0
+push word ptr 0
+
+; isup
+pop bx
+pop ax
+cmp ax,bx
+jle $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux SINON3
+pop ax
+cmp ax,0
+je SINON3
 
 ; iload -4
 push word ptr [bp-4]
 
-; iconst 5
-push word ptr 5
-
-; isub
-pop bx
+; iffaux SINON4
 pop ax
-sub ax,bx
-push ax
+cmp ax,0
+je SINON4
 
-;call max
-call max
-
-; iadd
-pop bx
-pop ax
-add ax,bx
-push ax
-
-;reserveRetour
-sub sp,2
-
-; iload -2
-push word ptr [bp-2]
+; iload -6
+push word ptr [bp-6]
 
 ; iconst 2
 push word ptr 2
@@ -239,24 +253,199 @@ pop ax
 imul bx
 push ax
 
-; iload -4
-push word ptr [bp-4]
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
 
-;call min
-call min
+FAIRE2:
+; iload -6
+push word ptr [bp-6]
 
-;call sup
-call sup
+; iconst 0
+push word ptr 0
+
+; idif
+pop bx
+pop ax
+cmp ax,bx
+je $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux FAIT2
+pop ax
+cmp ax,0
+je FAIT2
+
+; iload -6
+push word ptr [bp-6]
+
+; iconst 1
+push word ptr 1
+
+; isub
+pop bx
+pop ax
+sub ax,bx
+push ax
+
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
+
+FAIT2:
+; goto FSI4
+jmp FSI4
+
+SINON4:
+FAIRE3:
+; iload -6
+push word ptr [bp-6]
+
+; iconst 0
+push word ptr 0
+
+; idif
+pop bx
+pop ax
+cmp ax,bx
+je $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux FAIT3
+pop ax
+cmp ax,0
+je FAIT3
+
+; iload -6
+push word ptr [bp-6]
+
+; iconst 1
+push word ptr 1
+
+; isub
+pop bx
+pop ax
+sub ax,bx
+push ax
+
+; istore -6
+pop ax
+mov word ptr [bp-6],ax
+
+FAIT3:
+FSI4:
+; iconst 1
+push word ptr 1
+
+;ireturn 8
+pop ax
+mov [bp+8],ax
+
+; goto FSI3
+jmp FSI3
+
+SINON3:
+; iconst 1
+push word ptr 1
+
+; ineg
+pop bx
+neg bx
+push bx
+
+;ireturn 8
+pop ax
+mov [bp+8],ax
+
+FSI3:
+;fermeBloc 4
+leave
+ret 4
+
+debut :
+STARTUPCODE
+
+main:
+;ouvcBloc 14
+enter 14,0
+
+;reserveRetour
+sub sp,2
+
+;call lait
+call lait
+
+; istore -14
+pop ax
+mov word ptr [bp-14],ax
+
+;reserveRetour
+sub sp,2
+
+; iconst 22
+push word ptr 22
+
+; iload -14
+push word ptr [bp-14]
+
+;call caramel
+call caramel
 
 ; istore -8
 pop ax
 mov word ptr [bp-8],ax
 
+; lireEnt -10
+lea dx,[bp-10]
+push dx
+call lirent
+
 ; aLaLigne
 call ligsuiv
 
-; iload -6
-push word ptr [bp-6]
+;reserveRetour
+sub sp,2
+
+; iload -8
+push word ptr [bp-8]
+
+;reserveRetour
+sub sp,2
+
+; iload -10
+push word ptr [bp-10]
+
+; iconst 5
+push word ptr 5
+
+;call max
+call max
+
+;call max
+call max
+
+; iconst 2
+push word ptr 2
+
+; iadd
+pop bx
+pop ax
+add ax,bx
+push ax
+
+; istore -12
+pop ax
+mov word ptr [bp-12],ax
+
+; aLaLigne
+call ligsuiv
+
+; iload -12
+push word ptr [bp-12]
 
 ; ecrireEnt
 call ecrent
@@ -264,8 +453,8 @@ call ecrent
 ; aLaLigne
 call ligsuiv
 
-; iload -8
-push word ptr [bp-8]
+; iload -14
+push word ptr [bp-14]
 
 ; ecrireBool
 call ecrbool

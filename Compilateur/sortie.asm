3e9cd8ce6179b1d1030d7bbc4bd0af6f80269620
Jean Chorin <jchorin@insa-rennes.fr>
2015-04-20 14:32:45 +0200

    plus ou moins

--- Compilateur/sortie.asm
+++ Compilateur/sortie.asm
@@ -7,104 +7,15 @@ extrn ecrch:proc, ligsuiv:proc
 
 .CODE
 
-max:
-;ouvcBloc 6
-enter 6,0
+recommencer:
+;ouvcBloc 0
+enter 0,0
 
-; iload 6
-push word ptr [bp+6]
+; iload 5
+push word ptr [bp+5]
 
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-; iload -6
-push word ptr [bp-6]
-
-; iload 4
-push word ptr [bp+4]
-
-; isup
-pop bx
-pop ax
-cmp ax,bx
-jle $+6
-push -1
-jmp $+4
-push 0
-
-; iffaux SINON1
-pop ax
-cmp ax,0
-je SINON1
-
-; iload -6
-push word ptr [bp-6]
-
-;ireturn 8
-pop ax
-mov [bp+8],ax
-
-; goto FSI1
-jmp FSI1
-
-SINON1:
-; iload 4
-push word ptr [bp+4]
-
-;ireturn 8
-pop ax
-mov [bp+8],ax
-
-FSI1:
-;fermeBloc 4
-leave
-ret 4
-
-lait:
-;ouvcBloc 6
-enter 6,0
-
-; iconst 6
-push word ptr 6
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 42
-push word ptr 42
-
-; iinfegal
-pop bx
-pop ax
-cmp ax,bx
-jg $+6
-push -1
-jmp $+4
-push 0
-
-; istore -2
-pop ax
-mov word ptr [bp-2],ax
-
-; iload -6
-push word ptr [bp-6]
-
-;reserveRetour
-sub sp,2
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 5
-push word ptr 5
-
-;call max
-call max
+; iconst 1
+push word ptr 1
 
 ; iegal
 pop bx
@@ -115,109 +26,82 @@ push -1
 jmp $+4
 push 0
 
-; istore -4
+; iffaux SINON1
 pop ax
-mov word ptr [bp-4],ax
+cmp ax,0
+je SINON1
 
-FAIRE1:
-; iload -6
-push word ptr [bp-6]
+; iconst -1
+push word ptr -1
 
-; iconst 10
-push word ptr 10
+;ireturn 6
+pop ax
+mov [bp+6],ax
 
-; iinfegal
+; goto FSI1
+jmp FSI1
+
+SINON1:
+; iconst 0
+push word ptr 0
+
+;ireturn 6
+pop ax
+mov [bp+6],ax
+
+FSI1:
+;fermeBloc 2
+leave
+ret 2
+
+comparaison:
+;ouvcBloc 0
+enter 0,0
+
+; iload 6
+push word ptr [bp+6]
+
+; iload 4
+push word ptr [bp+4]
+
+; iinf
 pop bx
 pop ax
 cmp ax,bx
-jg $+6
+jge $+6
 push -1
 jmp $+4
 push 0
-
-; iffaux FAIT1
-pop ax
-cmp ax,0
-je FAIT1
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 1
-push word ptr 1
-
-; iadd
-pop bx
-pop ax
-add ax,bx
-push ax
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-FAIT1:
-; iload -2
-push word ptr [bp-2]
 
 ; iffaux SINON2
 pop ax
 cmp ax,0
 je SINON2
 
-; iconst 42
-push word ptr 42
+; ecrireChaine "MOINS"
+.DATA
+mess0 DB "MOINS$"
+.CODE
+lea dx,mess0
+push dx
+call ecrch
 
-; istore -6
+; iconst 0
+push word ptr 0
+
+;ireturn 8
 pop ax
-mov word ptr [bp-6],ax
-
-; iload -2
-push word ptr [bp-2]
-
-; istore -4
-pop ax
-mov word ptr [bp-4],ax
+mov [bp+8],ax
 
 ; goto FSI2
 jmp FSI2
 
 SINON2:
-FSI2:
-; iload -4
-push word ptr [bp-4]
-
-;ireturn 4
-pop ax
-mov [bp+4],ax
-
-;fermeBloc 0
-leave
-ret 0
-
-caramel:
-;ouvcBloc 6
-enter 6,0
-
 ; iload 6
 push word ptr [bp+6]
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
 
 ; iload 4
 push word ptr [bp+4]
-
-; istore -4
-pop ax
-mov word ptr [bp-4],ax
-
-; iload 6
-push word ptr [bp+6]
-
-; iconst 0
-push word ptr 0
 
 ; isup
 pop bx
@@ -233,113 +117,16 @@ pop ax
 cmp ax,0
 je SINON3
 
-; iload -4
-push word ptr [bp-4]
-
-; iffaux SINON4
-pop ax
-cmp ax,0
-je SINON4
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 2
-push word ptr 2
-
-; imul
-pop bx
-pop ax
-imul bx
-push ax
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-FAIRE2:
-; iload -6
-push word ptr [bp-6]
+; ecrireChaine "PLUS"
+.DATA
+mess1 DB "PLUS$"
+.CODE
+lea dx,mess1
+push dx
+call ecrch
 
 ; iconst 0
 push word ptr 0
-
-; idif
-pop bx
-pop ax
-cmp ax,bx
-je $+6
-push -1
-jmp $+4
-push 0
-
-; iffaux FAIT2
-pop ax
-cmp ax,0
-je FAIT2
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 1
-push word ptr 1
-
-; isub
-pop bx
-pop ax
-sub ax,bx
-push ax
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-FAIT2:
-; goto FSI4
-jmp FSI4
-
-SINON4:
-FAIRE3:
-; iload -6
-push word ptr [bp-6]
-
-; iconst 0
-push word ptr 0
-
-; idif
-pop bx
-pop ax
-cmp ax,bx
-je $+6
-push -1
-jmp $+4
-push 0
-
-; iffaux FAIT3
-pop ax
-cmp ax,0
-je FAIT3
-
-; iload -6
-push word ptr [bp-6]
-
-; iconst 1
-push word ptr 1
-
-; isub
-pop bx
-pop ax
-sub ax,bx
-push ax
-
-; istore -6
-pop ax
-mov word ptr [bp-6],ax
-
-FAIT3:
-FSI4:
-; iconst 1
-push word ptr 1
 
 ;ireturn 8
 pop ax
@@ -349,19 +136,23 @@ mov [bp+8],ax
 jmp FSI3
 
 SINON3:
-; iconst 1
-push word ptr 1
+; ecrireChaine "Reussi !"
+.DATA
+mess2 DB "Reussi !$"
+.CODE
+lea dx,mess2
+push dx
+call ecrch
 
-; ineg
-pop bx
-neg bx
-push bx
+; iconst -1
+push word ptr -1
 
 ;ireturn 8
 pop ax
 mov [bp+8],ax
 
 FSI3:
+FSI2:
 ;fermeBloc 4
 leave
 ret 4
@@ -370,66 +161,240 @@ debut :
 STARTUPCODE
 
 main:
-;ouvcBloc 14
-enter 14,0
+;ouvcBloc 24
+enter 24,0
 
-;reserveRetour
-sub sp,2
+; iconst -1
+push word ptr -1
 
-;call lait
-call lait
+; istore -22
+pop ax
+mov word ptr [bp-22],ax
+
+; iconst -1
+push word ptr -1
+
+; istore -24
+pop ax
+mov word ptr [bp-24],ax
+
+; iconst 1
+push word ptr 1
+
+; ineg
+pop bx
+neg bx
+push bx
 
 ; istore -14
 pop ax
 mov word ptr [bp-14],ax
 
-;reserveRetour
-sub sp,2
+; iconst 0
+push word ptr 0
 
-; iconst 22
-push word ptr 22
-
-; iload -14
-push word ptr [bp-14]
-
-;call caramel
-call caramel
-
-; istore -8
+; istore -16
 pop ax
-mov word ptr [bp-8],ax
+mov word ptr [bp-16],ax
 
-; lireEnt -10
-lea dx,[bp-10]
+; iconst 0
+push word ptr 0
+
+; istore -18
+pop ax
+mov word ptr [bp-18],ax
+
+; iconst 0
+push word ptr 0
+
+; istore -20
+pop ax
+mov word ptr [bp-20],ax
+
+; iconst 1
+push word ptr 1
+
+; istore -2
+pop ax
+mov word ptr [bp-2],ax
+
+FAIRE1:
+; iload -2
+push word ptr [bp-2]
+
+; iconst 0
+push word ptr 0
+
+; isup
+pop bx
+pop ax
+cmp ax,bx
+jle $+6
+push -1
+jmp $+4
+push 0
+
+; iload -2
+push word ptr [bp-2]
+
+; iconst 3
+push word ptr 3
+
+; iinf
+pop bx
+pop ax
+cmp ax,bx
+jge $+6
+push -1
+jmp $+4
+push 0
+
+; iand
+pop bx
+pop ax
+and ax,bx
+push ax
+
+; iffaux FAIT1
+pop ax
+cmp ax,0
+je FAIT1
+
+; ecrireChaine "Bienvenue dans le jeu plus ou moins"
+.DATA
+mess3 DB "Bienvenue dans le jeu plus ou moins$"
+.CODE
+lea dx,mess3
+push dx
+call ecrch
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "Pour jouer : 1"
+.DATA
+mess4 DB "Pour jouer : 1$"
+.CODE
+lea dx,mess4
+push dx
+call ecrch
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "Voir les meilleurs scores : 2"
+.DATA
+mess5 DB "Voir les meilleurs scores : 2$"
+.CODE
+lea dx,mess5
+push dx
+call ecrch
+
+; aLaLigne
+call ligsuiv
+
+; lireEnt -2
+lea dx,[bp-2]
 push dx
 call lirent
 
 ; aLaLigne
 call ligsuiv
 
-;reserveRetour
-sub sp,2
+; iload -2
+push word ptr [bp-2]
 
-; iload -8
-push word ptr [bp-8]
+; iconst 1
+push word ptr 1
 
-;reserveRetour
-sub sp,2
+; iegal
+pop bx
+pop ax
+cmp ax,bx
+jne $+6
+push -1
+jmp $+4
+push 0
 
-; iload -10
-push word ptr [bp-10]
+; iffaux SINON4
+pop ax
+cmp ax,0
+je SINON4
 
-; iconst 5
-push word ptr 5
+FAIRE2:
+; iload -22
+push word ptr [bp-22]
 
-;call max
-call max
+; iffaux FAIT2
+pop ax
+cmp ax,0
+je FAIT2
 
-;call max
-call max
+; ecrireChaine "Donnez la borne de debut (ex: 0)"
+.DATA
+mess6 DB "Donnez la borne de debut (ex: 0)$"
+.CODE
+lea dx,mess6
+push dx
+call ecrch
 
-; iconst 2
-push word ptr 2
+; lireEnt -6
+lea dx,[bp-6]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "Donnez la borne de fin (ex: 100)"
+.DATA
+mess7 DB "Donnez la borne de fin (ex: 100)$"
+.CODE
+lea dx,mess7
+push dx
+call ecrch
+
+; lireEnt -8
+lea dx,[bp-8]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+; iconst 10
+push word ptr 10
+
+; istore -10
+pop ax
+mov word ptr [bp-10],ax
+
+; iconst 0
+push word ptr 0
+
+; istore -12
+pop ax
+mov word ptr [bp-12],ax
+
+FAIRE3:
+; iload -24
+push word ptr [bp-24]
+
+; inot
+pop bx
+not bx
+push bx
+
+; iffaux FAIT3
+pop ax
+cmp ax,0
+je FAIT3
+
+; iload -12
+push word ptr [bp-12]
+
+; iconst 1
+push word ptr 1
 
 ; iadd
 pop bx
@@ -441,8 +406,13 @@ push ax
 pop ax
 mov word ptr [bp-12],ax
 
-; aLaLigne
-call ligsuiv
+; ecrireChaine "Iteration "
+.DATA
+mess8 DB "Iteration $"
+.CODE
+lea dx,mess8
+push dx
+call ecrch
 
 ; iload -12
 push word ptr [bp-12]
@@ -453,12 +423,263 @@ call ecrent
 ; aLaLigne
 call ligsuiv
 
+; ecrireChaine "Votre numero : "
+.DATA
+mess9 DB "Votre numero : $"
+.CODE
+lea dx,mess9
+push dx
+call ecrch
+
+; lireEnt -14
+lea dx,[bp-14]
+push dx
+call lirent
+
+;reserveRetour
+sub sp,2
+
+; iload -10
+push word ptr [bp-10]
+
 ; iload -14
 push word ptr [bp-14]
 
-; ecrireBool
-call ecrbool
+;call comparaison
+call comparaison
 
+; istore -24
+pop ax
+mov word ptr [bp-24],ax
+
+FAIT3:
+; iload -12
+push word ptr [bp-12]
+
+; iload -20
+push word ptr [bp-20]
+
+; isup
+pop bx
+pop ax
+cmp ax,bx
+jle $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux SINON5
+pop ax
+cmp ax,0
+je SINON5
+
+; iload -12
+push word ptr [bp-12]
+
+; iload -18
+push word ptr [bp-18]
+
+; isup
+pop bx
+pop ax
+cmp ax,bx
+jle $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux SINON6
+pop ax
+cmp ax,0
+je SINON6
+
+; iload -12
+push word ptr [bp-12]
+
+; iload -16
+push word ptr [bp-16]
+
+; isup
+pop bx
+pop ax
+cmp ax,bx
+jle $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux SINON7
+pop ax
+cmp ax,0
+je SINON7
+
+; iload -12
+push word ptr [bp-12]
+
+; istore -16
+pop ax
+mov word ptr [bp-16],ax
+
+; goto FSI7
+jmp FSI7
+
+SINON7:
+; iload -12
+push word ptr [bp-12]
+
+; istore -18
+pop ax
+mov word ptr [bp-18],ax
+
+FSI7:
+; goto FSI6
+jmp FSI6
+
+SINON6:
+; iload -12
+push word ptr [bp-12]
+
+; istore -20
+pop ax
+mov word ptr [bp-20],ax
+
+FSI6:
+; goto FSI5
+jmp FSI5
+
+SINON5:
+FSI5:
+; ecrireChaine "Voulez-vous recommencer ? (Oui 1/Non 2"
+.DATA
+mess10 DB "Voulez-vous recommencer ? (Oui 1/Non 2$"
+.CODE
+lea dx,mess10
+push dx
+call ecrch
+
+; lireEnt -4
+lea dx,[bp-4]
+push dx
+call lirent
+
+; aLaLigne
+call ligsuiv
+
+;reserveRetour
+sub sp,2
+
+; iload -4
+push word ptr [bp-4]
+
+;call recommencer
+call recommencer
+
+; istore -22
+pop ax
+mov word ptr [bp-22],ax
+
+FAIT2:
+; goto FSI4
+jmp FSI4
+
+SINON4:
+; iload -2
+push word ptr [bp-2]
+
+; iconst 2
+push word ptr 2
+
+; iegal
+pop bx
+pop ax
+cmp ax,bx
+jne $+6
+push -1
+jmp $+4
+push 0
+
+; iffaux SINON8
+pop ax
+cmp ax,0
+je SINON8
+
+; ecrireChaine "Meilleurs scores"
+.DATA
+mess11 DB "Meilleurs scores$"
+.CODE
+lea dx,mess11
+push dx
+call ecrch
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "1er : "
+.DATA
+mess12 DB "1er : $"
+.CODE
+lea dx,mess12
+push dx
+call ecrch
+
+; iload -16
+push word ptr [bp-16]
+
+; ecrireEnt
+call ecrent
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "2e : "
+.DATA
+mess13 DB "2e : $"
+.CODE
+lea dx,mess13
+push dx
+call ecrch
+
+; iload -18
+push word ptr [bp-18]
+
+; ecrireEnt
+call ecrent
+
+; aLaLigne
+call ligsuiv
+
+; ecrireChaine "3e : "
+.DATA
+mess14 DB "3e : $"
+.CODE
+lea dx,mess14
+push dx
+call ecrch
+
+; iload -20
+push word ptr [bp-20]
+
+; ecrireEnt
+call ecrent
+
+; aLaLigne
+call ligsuiv
+
+; goto FSI8
+jmp FSI8
+
+SINON8:
+; ecrireChaine "Mauvaise touche. Au revoir !"
+.DATA
+mess15 DB "Mauvaise touche. Au revoir !$"
+.CODE
+lea dx,mess15
+push dx
+call ecrch
+
+FSI8:
+FSI4:
+FAIT1:
 ; queue
 nop
 exitcode
